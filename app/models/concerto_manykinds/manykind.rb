95d6dba734b5c7197f0dc3c0d8ce00d66c806361
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-06-15 12:02:00 -0800

    validate against existing primary kind

--- app/models/concerto_manykinds/manykind.rb
+++ app/models/concerto_manykinds/manykind.rb
@@ -12,6 +12,16 @@ module ConcertoManykinds
     validates_uniqueness_of :kind_id, :scope => [:template_id, :field_id]
 
     # TODO dont allow record if already exists in position field mapping
+    validate :cannot_be_same_as_primary
+
+    def cannot_be_same_as_primary
+      if template_id.present? && field_id.present? && kind_id.present?
+        p = Position.where(:template_id => template_id, :field_id => field_id).first
+        if !p.nil? and p.field.kind_id == kind_id
+          errors.add(:kind_id, 'already mapped via template definition')
+        end
+      end
+    end
 
     def self.form_attributes
       attributes = [:field_id, :template_id,  :kind_id]
