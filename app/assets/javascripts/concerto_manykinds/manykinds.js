937aa79a2ac5cefe34ac96015482594440b6eefd
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-03-02 11:14:38 -0900

    isolate namespace, load on ready

--- app/assets/javascripts/concerto_manykinds/manykinds.js
+++ app/assets/javascripts/concerto_manykinds/manykinds.js
@@ -1,23 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var manykinds_handlers_initialized = false;
 
-function attachConcertoManykindsHandlers() {
-  if (!manykinds_handlers_initialized) {
-    $('ul.manykinds-list').on('ajax:success', 'a[data-method="delete"]', function (e, data, status, xhr) {
-console.debug('firing');
-      $(this).parent('li').hide();
-    });
+var ConcertoManykinds = {
+  _initialized: false,
 
-    $('form[id="new_manykind"]').on('ajax:success', function (e, data, status, xhr) {
-      // show the new item
-      $("#manykinds_fld" + data["field_id"]).append(data["item_html"]);
-    });
+  initHandlers: function () {
+    if (!ConcertoManykinds._initialized) {
+      $('ul.manykinds-list').on('ajax:success', 'a[data-method="delete"]', function (e, data, status, xhr) {
+        $(this).parent('li').hide();
+      });
 
-    console.debug('manykinds handlers initialized');
-    manykinds_handlers_initialized = true;
+      $('form[id="new_manykind"]').on('ajax:success', function (e, data, status, xhr) {
+        // show the new item
+        $("#manykinds_fld" + data["field_id"]).append(data["item_html"]);
+      });
+      ConcertoManykinds._initialized = true;
+    }
   }
-}
+};
 
-$(document).on('turbolinks:load', attachConcertoManykindsHandlers);
-
+$(document).ready(ConcertoManykinds.initHandlers);
+$(document).on('turbolinks:load', ConcertoManykinds.initHandlers);
